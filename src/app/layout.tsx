e4e28692c6c35c354105a12e5d2cdbc19e860d15
USpiri <urielspiridione135@gmail.com>
2024-03-18 20:09:29 -0300

    ✨ Feat: Create sidebar component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Sidebar } from "@/components/Sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,9 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${inter.className} dark bg-neutral-950 text-neutral-100`}
+        className={`${inter.className} dark flex bg-neutral-950 text-neutral-100`}
       >
-        <aside></aside>
+        <Sidebar />
         {children}
       </body>
     </html>
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,35 @@
+"use client";
+import { useState } from "react";
+import { Button } from "./ui/button";
+import { Menu } from "lucide-react";
+import { cn } from "@/lib/utils";
+import "./sidebar.css";
+
+export const Sidebar = () => {
+  const [open, setOpen] = useState(true);
+
+  return (
+    <aside className="fixed flex sm:relative">
+      <div
+        className={cn(
+          "sidebar h-screen overflow-hidden border-r bg-neutral-950",
+          open && "open",
+        )}
+      >
+        <div className="w-64">
+          <p className="text-sm opacity-50">Cooming soon...</p>
+        </div>
+      </div>
+      <div>
+        <Button
+          onClick={() => setOpen(!open)}
+          variant="outline"
+          size="icon"
+          className={cn("absolute top-2 sm:ml-2", open ? "-ml-12" : "ml-2")}
+        >
+          <Menu className="h-5 w-5" />
+        </Button>
+      </div>
+    </aside>
+  );
+};
