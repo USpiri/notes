863a3588a7981d34e287be6abd5df2870f58aca2
USpiri <urielspiridione135@gmail.com>
2024-03-18 20:10:38 -0300

    🐛 Fix: missing import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { Sidebar } from "@/components/Sidebar";
+import { Sidebar } from "@/components/sidebar/Sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
 
