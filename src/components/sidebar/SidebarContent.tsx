9fd2c9fc84e909a2417266f7d9664a49acb1e321
USpiri <urielspiridione135@gmail.com>
2024-06-12 21:13:20 -0300

    ✨ Feat: Search and filter notes

--- src/components/sidebar/SidebarContent.tsx
+++ src/components/sidebar/SidebarContent.tsx
@@ -8,15 +8,55 @@ import {
 } from "@minoru/react-dnd-treeview";
 import { DndProvider } from "react-dnd";
 import { SidebarNode } from "./SidebarNode";
+import { Search } from "lucide-react";
+import { ChangeEvent, useEffect, useState } from "react";
 
 export const SidebarContent = () => {
-  const tree = useNoteStore((s) => s.folders);
+  const treeStore = useNoteStore((s) => s.folders);
   const updateFolders = useNoteStore((s) => s.updateFolders);
 
+  const [tree, setTree] = useState<NodeModel[]>([]);
+  const [searchParam, setSearchParam] = useState("");
+
   const handleDrop = (node: NodeModel[]) => updateFolders(node as TreeNode[]);
+  const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
+    const search = event.target.value.trim().toLowerCase();
+    setSearchParam(search);
+    if (search.length) {
+      filterTree(search);
+    } else {
+      setTree(treeStore);
+    }
+  };
+
+  const filterTree = (search: string) => {
+    setTree(
+      treeStore
+        .filter((t) => t.text.toLowerCase().includes(search) && !t.droppable)
+        .map((t) => ({ ...t, parent: "root" })),
+    );
+  };
+
+  useEffect(() => {
+    if (searchParam.length) {
+      filterTree(searchParam);
+    } else {
+      setTree(treeStore);
+    }
+  }, [treeStore]);
 
   return (
     <DndProvider backend={MultiBackend} options={getBackendOptions()}>
+      <div>
+        <label className="m-1 flex items-center gap-2 rounded-md border border-neutral-700 px-2 py-1 text-xs text-neutral-300">
+          <Search className="h-4 w-4" />
+          <input
+            placeholder="Search..."
+            onChange={handleSearch}
+            className="w-full bg-transparent py-1 placeholder-neutral-400 outline-none placeholder:font-medium"
+          />
+        </label>
+      </div>
       <div className="flex h-full flex-col gap-2 overflow-y-auto">
         <Tree
           tree={tree}
